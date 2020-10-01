cbb06c81086ceabe41198e649e3a82b1a44b6167
tangruiwen.trw <tangruiwen.mmh1103@bytedance.com>
2020-10-01 09:57:06 +0800

    Add PlatformWindows implement

---
+++ libs/txt/platform/PlatformWindows.cpp
@@ -0,0 +1,13 @@
+//
+// Created by TangRuiwen on 2020/10/01.
+//
+
+#include <txt/Platform.h>
+
+namespace txt {
+
+std::vector<std::string> Platform::GetDefaultFontFamilies() {
+    return {"Segoe UI", "Arial"};
+}
+
+} // namespace txt
