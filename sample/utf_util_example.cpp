38c755836cf4f1effd6ec943798271df7f5172af
tangruiwen.trw <tangruiwen.mmh1103@bytedance.com>
2020-10-11 19:04:13 +0800

    Change code to use utf8

--- sample/utf_util_example.cpp
+++ sample/utf_util_example.cpp
@@ -1,4 +1,4 @@
-﻿//
+//
 // Created by TangRuiwen on 2020/10/11.
 //
 
@@ -8,8 +8,9 @@
 #include <string>
 
 int main(int argc, const char** argv) {
-    char text[] = "fine world \xe4\xb8\xad";
-    //"fine world \xe0\xa4\xa8\xe0\xa4\xae\xe0\xa4\xb8\xe0\xa5\x8d\xe0\xa4\xa4\xe0\xa5\x87";
+    char text[] = 
+    // "fine world \xe4\xb8\xad";
+    "fine world \xe0\xa4\xa8\xe0\xa4\xae\xe0\xa4\xb8\xe0\xa5\x8d\xe0\xa4\xa4\xe0\xa5\x87";
 
     int count = txt::UTF::CountUTF8(text, sizeof(text));
     std::cout << "raw text = " << text << std::endl;
@@ -17,12 +18,13 @@ int main(int argc, const char** argv) {
 
     const char* ptr = text;
     const char* end = text + sizeof(text);
-
+    const char* p = ptr;
     while (ptr != end) {
         int32_t ch = txt::UTF::NextUTF8(&ptr, end);
-
-        std::string char_str{(char*)std::addressof(ch), sizeof(ch)};
+        uint32_t length = ptr - p;
+        std::string char_str{p, length};
         std::cout << "next ch = " << char_str << std::endl;
+        p = ptr;
     }
 
     return 0;
